8d6b8145cc9e5c166f7198a98334904cfd768359
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-02-25 18:16:07 +0530

    Random scan from beets db

--- src/utils/randomscan.py
+++ src/utils/randomscan.py
@@ -7,62 +7,35 @@ import eyed3
 import pyprind
 import os
 import cPickle
-import sys
 from numpy.random import normal as random
 import yaml
+from setupdb import reset
 
 config = yaml.load(open("../config.yaml").read())
 
-if len(sys.argv) != 2:
-    print("Error in usage")
-    print("use: python scan.py <directory>")
-    sys.exit(1)
+beets_db_file = config["beets_db"]
+blackbird_db_file = config["blackbird_db"]
 
-db_file = config["db"]
+# Reset db
+reset(blackbird_db_file, features=True)
 
-# Performs a hard scan and library reset
 
-try:
-    os.remove(db_file)
-except OSError:
-    pass
-
-conn = sqlite3.connect(db_file)
+# Read ids
+conn = sqlite3.connect(beets_db_file)
 cur = conn.cursor()
+cur.execute("SELECT id FROM items")
+data = cur.fetchall()
+conn.close()
 
-to_scan = unicode(sys.argv[1])
-songs = []
+# Write random data
+coords_data = []
+for idx in xrange(len(data)):
+    coords_data.append((data[idx][0],
+                        random(),
+                        random()))
 
-for root, _, files in os.walk(to_scan):
-    for filename in files:
-        if filename.endswith(".mp3"):
-            songs.append(os.path.join(root, filename))
-
-print("Reading data...")
-
-bar = pyprind.ProgBar(len(songs))
-
-cmd = "CREATE TABLE songs (id INTEGER PRIMARY KEY, title TEXT, artist TEXT, album TEXT, path TEXT, x FLOAT, y FLOAT)"
-cur.execute(cmd)
-
-songs_data = []
-
-for idx, song in enumerate(songs):
-    try:
-        audio_data = eyed3.load(song)
-        songs_data.append((idx,
-                           audio_data.tag.title,
-                           audio_data.tag.artist,
-                           audio_data.tag.album,
-                           song,
-                           random(),
-                           random()))
-    except Exception as e:
-        print e
-        sys.exit(1)
-    bar.update()
-
-cur.executemany("INSERT INTO songs VALUES (?, ?, ?, ?, ?, ?, ?)", songs_data)
-
+conn = sqlite3.connect(blackbird_db_file)
+cur = conn.cursor()
+cur.executemany("INSERT INTO coords VALUES (?, ?, ?)", coords_data)
 conn.commit()
 conn.close()
