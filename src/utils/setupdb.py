5fc291d59eea60578ec3dc4250a3ac37d39937e8
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-02-29 14:51:11 +0530

    New db structure

--- src/utils/setupdb.py
+++ src/utils/setupdb.py
@@ -6,28 +6,21 @@ import sqlite3
 import os
 
 
-def reset(file_path, features=False):
+def reset(file_path):
     """
     Create the database if not present.
-    If present, clean up the coords and (optionally) features
+    If present, clean it up
     """
 
     if os.path.isfile(file_path):
         conn = sqlite3.connect(file_path)
         cur = conn.cursor()
         cur.execute("DELETE FROM coords")
-
-        if features:
-            cur.execute("DELETE FROM features")
-
         conn.commit()
         conn.close()
     else:
         conn = sqlite3.connect(file_path)
         cur = conn.cursor()
-
         cur.execute("CREATE TABLE coords (id INTEGER PRIMARY KEY, x REAL, y REAL)")
-        cur.execute("CREATE TABLE features (id INTEGER PRIMARY KEY, feature BLOB)")
-
         conn.commit()
         conn.close()
