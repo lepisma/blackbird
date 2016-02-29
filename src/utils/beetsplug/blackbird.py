d2b839e0e82c56c8c284816b9a564373b7dd5324
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2016-02-29 11:58:47 +0530

    Add intermediate feature extraction in beets plugin

--- src/utils/beetsplug/blackbird.py
+++ src/utils/beetsplug/blackbird.py
@@ -3,9 +3,16 @@ Module for pipelining media management from beets to blackbird
 """
 
 from beets.plugins import BeetsPlugin
+from beets.ui import Subcommand, decargs
 from beets import config
 import sqlite3
 from numpy.random import normal as random
+import pyprind
+import librosa
+import cPickle
+import numpy as np
+from skimage.measure import block_reduce
+import sys
 
 
 class Blackbird(BeetsPlugin):
@@ -16,6 +23,57 @@ class Blackbird(BeetsPlugin):
     def __init__(self):
         super(Blackbird, self).__init__()
         self.register_listener("import", self.db_change)
+
+    def commands(self):
+        """
+        Commands for generating and importing features
+        """
+
+        def get_mfcc(song):
+            y, sr = librosa.load(song.encode(sys.getfilesystemencoding()))
+            mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+            return block_reduce(mfcc, (1, 100), np.mean)
+
+        # Generate sequential features
+        features_cmd = Subcommand("features",
+                                  help="Generate sequential acoustic features [blackbird]")
+
+        def features_func(lib, opts, args):
+            filtered_items = lib.items(decargs(args))
+
+            if len(filtered_items) == 0:
+                print("Query didn't match any item")
+                return
+
+            features_file = config["blackbird"]["seq_features"].get("unicode")
+            features = cPickle.load(open(features_file, "rb"))
+
+            print("Finding features...")
+            bar = pyprind.ProgBar(len(filtered_items))
+            for item in filtered_items:
+                if item.id not in features:
+                    try:
+                        data = get_mfcc(item.path)
+                        features[item.id] = data
+                    except Exception as e:
+                        print e
+                bar.update()
+            cPickle.dump(features, open(features_file, "wb"), protocol=cPickle.HIGHEST_PROTOCOL)
+
+        # Import features as coordinates
+        coords_cmd = Subcommand("coords",
+                                help="Generate coordinates based on calculated features [blackbird]")
+        coords_cmd.parser.add_option("-t",
+                                     "--type",
+                                     help="Type of vectorization [mean (default), lstm]")
+
+        def coords_func(lib, opts, args):
+            pass
+
+        coords_cmd.func = coords_func
+        features_cmd.func = features_func
+
+        return [features_cmd, coords_cmd]
 
     def db_change(self, lib):
         self.register_listener("cli_exit", self.update_coordinates)
